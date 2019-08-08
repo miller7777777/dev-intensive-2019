f0423296885a446746f0bbc0865337f75054edfe
Miller7777777 <miller777@mail.ru>
2019-08-09 02:50:14 +0300

    Занятие 3 закончено.

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -1,6 +1,6 @@
 package ru.skillbranch.devintensive.models
 
-class Bender (var status: Status = Status.NORMAL, val question: Question = Question.NAME){
+class Bender (var status: Status = Status.NORMAL, var question: Question = Question.NAME){
 
     fun askQuestion(): String = when (question){
         Question.NAME -> Question.NAME.question
@@ -14,21 +14,55 @@ class Bender (var status: Status = Status.NORMAL, val question: Question = Quest
 
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
 
+        return if (question.answers.contains(answer)){
+            question = question.nextQuestion()
+            "Отлично - это правильный ответ!\n${question.question}" to status.color
+        }else{
+            status = status.nextStatus()
+            "Это неправильный ответ!\n" +
+                    "${question.question}" to status.color
+        }
+
     }
 
     enum class Status(val color : Triple<Int, Int, Int>){
         NORMAL(Triple(255, 255, 255)),
         WARNING(Triple(255, 120, 0)),
         DANGER(Triple(255, 60, 60)),
-        CRITICAL(Triple(255, 255, 0))
+        CRITICAL(Triple(255, 255, 0));
+
+        fun nextStatus(): Status{
+            return if (this.ordinal < values().lastIndex){
+
+                values()[this.ordinal + 1]
+            }else{
+                values()[0]
+            }
+        }
     }
 
-    enum class Question(val question: String, val answers: List){
-        NAME("Как меня зовут?", listOf("бендер", "bender")),
-        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")),
-        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")),
-        BDAY("Когда меня создали?", listOf("2993")),
-        SERIAL("Мой серийный номер?", listOf("2716057")),
-        IDLE("На этом все, вопросов больше нет", listOf())
+
+
+    enum class Question(val question: String, val answers: List<String>){
+        NAME("Как меня зовут?", listOf("бендер", "bender")) {
+            override fun nextQuestion(): Question = PROFESSION
+        },
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
+            override fun nextQuestion(): Question = MATERIAL
+        },
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
+            override fun nextQuestion(): Question = BDAY
+        },
+        BDAY("Когда меня создали?", listOf("2993")) {
+            override fun nextQuestion(): Question = SERIAL
+        },
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
+            override fun nextQuestion(): Question = IDLE
+        },
+        IDLE("На этом все, вопросов больше нет", listOf()) {
+            override fun nextQuestion(): Question = PROFESSION
+        };
+
+        abstract fun nextQuestion():Question
     }
 }
